35fba46a56bf461b04834293d49309ef5bd72d9d
Revathy-2001 <77260440+Revathy-2001@users.noreply.github.com>
2021-01-11 21:57:55 +0530

    Add files via upload

---
+++ timeloop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StuckInTimeLoop{
+    public static void main(String[] args){
+      Scanner kbd = new Scanner(System.in);
+      int n = kbd.nextInt();
+      for(int i = 1;i <= n;i++)
+        System.out.println(i + " Abracadabra");
+    }
+}
